2b1a5f56b6b0a35a8a7651469e2a4744e69d4538
NovemberXXI <NovemberXXI.Business@gmail.com>
2023-02-22 10:31:55 +0200

    new update and about

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const navLink = document.querySelector(".nav-link");
+
+hamburger.addEventListener("click", () => {
+  navLink.classList.toggle("hide");
+});
+
 const swiper = new Swiper(".swiper", {
   autoplay: {
     delay: 2000,
